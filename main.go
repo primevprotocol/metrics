031010f8eb2f7a9913f9d21e7c44d1428c1f78db
Kartik Chopra <k5chopra@edu.uwaterloo.ca>
2023-07-28 00:12:16 -0400

    Adds more detailed unmarshalling requirements.

--- main.go
+++ main.go
@@ -33,9 +33,63 @@ type ResponseData struct {
 		ExtraData string `json:"extraData"`
 	} `json:"result"`
 }
+type Transaction struct {
+	BlockHash            string   `json:"blockHash"`
+	BlockNumber          string   `json:"blockNumber"`
+	From                 string   `json:"from"`
+	Gas                  string   `json:"gas"`
+	GasPrice             string   `json:"gasPrice"`
+	MaxFeePerGas         string   `json:"maxFeePerGas"`
+	MaxPriorityFeePerGas string   `json:"maxPriorityFeePerGas"`
+	Hash                 string   `json:"hash"`
+	Input                string   `json:"input"`
+	Nonce                string   `json:"nonce"`
+	To                   string   `json:"to"`
+	TransactionIndex     string   `json:"transactionIndex"`
+	Value                string   `json:"value"`
+	Type                 string   `json:"type"`
+	AccessList           []string `json:"accessList"`
+	ChainId              string   `json:"chainId"`
+	V                    string   `json:"v"`
+	R                    string   `json:"r"`
+	S                    string   `json:"s"`
+}
 
-type ResponseDataBody struct {
-	Result string `json:"result"`
+type Withdrawal struct {
+	Index          string `json:"index"`
+	ValidatorIndex string `json:"validatorIndex"`
+	Address        string `json:"address"`
+	Amount         string `json:"amount"`
+}
+
+type Payload struct {
+	Jsonrpc string `json:"jsonrpc"`
+	ID      int    `json:"id"`
+	Result  struct {
+		BaseFeePerGas    string        `json:"baseFeePerGas"`
+		Difficulty       string        `json:"difficulty"`
+		ExtraData        string        `json:"extraData"`
+		GasLimit         string        `json:"gasLimit"`
+		GasUsed          string        `json:"gasUsed"`
+		Hash             string        `json:"hash"`
+		LogsBloom        string        `json:"logsBloom"`
+		Miner            string        `json:"miner"`
+		MixHash          string        `json:"mixHash"`
+		Nonce            string        `json:"nonce"`
+		Number           string        `json:"number"`
+		ParentHash       string        `json:"parentHash"`
+		ReceiptsRoot     string        `json:"receiptsRoot"`
+		Sha3Uncles       string        `json:"sha3Uncles"`
+		Size             string        `json:"size"`
+		StateRoot        string        `json:"stateRoot"`
+		Timestamp        string        `json:"timestamp"`
+		TotalDifficulty  string        `json:"totalDifficulty"`
+		Transactions     []Transaction `json:"transactions"`
+		TransactionsRoot string        `json:"transactionsRoot"`
+		Uncles           []string      `json:"uncles"`
+		Withdrawals      []Withdrawal  `json:"withdrawals"`
+		WithdrawalsRoot  string        `json:"withdrawalsRoot"`
+	} `json:"result"`
 }
 
 func main() {
@@ -152,7 +206,7 @@ func main() {
 		responseDataBody := processData(requestDataBlockByNumber, url)
 
 		// Decode the response JSON
-		var responseBlock ResponseDataBody
+		var responseBlock Payload
 		log.Info().Msg(string(responseDataBody))
 
 		err = json.Unmarshal(responseDataBody, &responseBlock)
@@ -160,6 +214,7 @@ func main() {
 			log.Fatal().Err(err).Msg("Error decoding response JSON")
 		}
 
+		time.Sleep(1 * time.Second)
 		// Increment the params value
 		blockNumber++
 	}
