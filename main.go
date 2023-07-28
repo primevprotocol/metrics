3a72156b60aefed02b9de96308d8350bb157fa25
Kartik Chopra <k5chopra@edu.uwaterloo.ca>
2023-07-28 00:16:45 -0400

    remove offending accesslist.

--- main.go
+++ main.go
@@ -34,25 +34,24 @@ type ResponseData struct {
 	} `json:"result"`
 }
 type Transaction struct {
-	BlockHash            string   `json:"blockHash"`
-	BlockNumber          string   `json:"blockNumber"`
-	From                 string   `json:"from"`
-	Gas                  string   `json:"gas"`
-	GasPrice             string   `json:"gasPrice"`
-	MaxFeePerGas         string   `json:"maxFeePerGas"`
-	MaxPriorityFeePerGas string   `json:"maxPriorityFeePerGas"`
-	Hash                 string   `json:"hash"`
-	Input                string   `json:"input"`
-	Nonce                string   `json:"nonce"`
-	To                   string   `json:"to"`
-	TransactionIndex     string   `json:"transactionIndex"`
-	Value                string   `json:"value"`
-	Type                 string   `json:"type"`
-	AccessList           []string `json:"accessList"`
-	ChainId              string   `json:"chainId"`
-	V                    string   `json:"v"`
-	R                    string   `json:"r"`
-	S                    string   `json:"s"`
+	BlockHash            string `json:"blockHash"`
+	BlockNumber          string `json:"blockNumber"`
+	From                 string `json:"from"`
+	Gas                  string `json:"gas"`
+	GasPrice             string `json:"gasPrice"`
+	MaxFeePerGas         string `json:"maxFeePerGas"`
+	MaxPriorityFeePerGas string `json:"maxPriorityFeePerGas"`
+	Hash                 string `json:"hash"`
+	Input                string `json:"input"`
+	Nonce                string `json:"nonce"`
+	To                   string `json:"to"`
+	TransactionIndex     string `json:"transactionIndex"`
+	Value                string `json:"value"`
+	Type                 string `json:"type"`
+	ChainId              string `json:"chainId"`
+	V                    string `json:"v"`
+	R                    string `json:"r"`
+	S                    string `json:"s"`
 }
 
 type Withdrawal struct {
